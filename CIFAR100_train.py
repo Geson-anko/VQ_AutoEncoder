fa931b58657026160a354b1ed996d83f7232c117
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2021-08-12 14:43:25 +0900

    After VQ training

--- CIFAR100_train.py
+++ CIFAR100_train.py
@@ -6,7 +6,7 @@ import pytorch_lightning as pl
 from torch.utils import data as DataUtil
 
 from CIFAR100_model import VQ_AutoEncoder
-from hparams import CIFAR100_default as hparams
+from hparams import CIFAR100_afterVQ as hparams
 
 import os
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
@@ -21,11 +21,14 @@ EPOCHS = 10000
 batch_size= 1024 *8
 model = VQ_AutoEncoder(hparams)
 data_loader = DataUtil.DataLoader(data_set,batch_size,shuffle=True,num_workers=0,pin_memory=True,drop_last=True)
-
+#%%
+model.encoder.load_state_dict(torch.load('params/CIFAR100_encoder.pth'))
+model.decoder.load_state_dict(torch.load('params/CIFAR100_decoder.pth'))
 #%%
 if __name__ == '__main__':
     trainer = pl.Trainer(gpus=1,num_nodes=1,precision=16,max_epochs=EPOCHS)
     trainer.fit(model,data_loader)
+    torch.save(model.state_dict(),f'params/{model.model_name}.pth')
 
 
 
---
+++ hparams/CIFAR100_afterVQ.py
@@ -0,0 +1,29 @@
+"""
+This is a hyper parameters of VQ_AutoEncoder for CIFAR100 
+"""
+
+max_view_imgs = 16
+view_interval = 50
+
+model_name:str = 'CIFAR100_afterVQ'
+lr:float = 0.00001
+momentum:float = 0.9
+num_quantizing:int = 500
+quantizing_dim:int = 32
+
+encoder_channels = (8,16,32)
+res_kernel = 5
+res_num_layers = 2
+
+class encoder_hparams:
+    model_name = model_name + '_encoder'
+    quantizing_dim = quantizing_dim
+    encoder_channels = encoder_channels
+    ch0,ch1,ch2 = encoder_channels
+    res_kernel,res_num_layers = res_kernel,res_num_layers
+
+class decoder_hparams:
+    model_name = model_name + '_decoder'
+    quantizing_dim = quantizing_dim
+    encoder_channels = encoder_channels
+    res_kernel,res_num_layers = res_kernel,res_num_layers
